58ca3ac94207e98c59f80ea4a80306a66a8c94e9
NickYxy <bing900713@gmail.com>
2023-02-28 10:02:36 +0800

    添加归并排序

--- 2570.合并两个二维数组-求和法.cpp
+++ 2570.合并两个二维数组-求和法.cpp
@@ -22,5 +22,35 @@ public:
         return ans;
     }
 };
+
+// 归并排序会更好一些，速度也更快，空间占用也小
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        vector<vector<int>> ans;
+        int n1 = nums1.size(), n2 = nums2.size(), i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i][0] == nums2[j][0]) {
+                ans.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i ++ ; j ++ ;
+            } else if (nums1[i][0] < nums2[j][0]) {
+                ans.push_back(nums1[i]);
+                i ++ ;
+            } else {
+                ans.push_back(nums2[j]);
+                j ++ ;
+            }
+        }
+        while (i < n1) {
+            ans.push_back(nums1[i]);
+            i ++ ;
+        }
+        while (j < n2) {
+            ans.push_back(nums2[j]);
+            j ++ ;
+        }
+        return ans;
+    }
+};
 // @lc code=end
 
