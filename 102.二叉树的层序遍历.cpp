b5fff25d3570e27bd25e4e88faa30fde27eb286d
NickYxy <bing900713@gmail.com>
2023-03-01 15:56:35 +0800

    二叉树层序遍历--模板题

---
+++ 102.二叉树的层序遍历.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=cpp
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (q.size()) {
+            int qlen = q.size();
+            vector<int> path;
+            for (int i = 0; i < qlen; i ++) {
+                auto node = q.front();
+                q.pop();
+                path.push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            ans.push_back(path);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
