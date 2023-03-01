93e41ee637258f1fa5098021a893eaab88fb1b01
NickYxy <bing900713@gmail.com>
2023-03-01 20:00:06 +0800

    用递归的方法来判断p和q的位置

---
+++ 236.二叉树的最近公共祖先.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=cpp
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return NULL;
+        if (p->val == root->val || q->val == root->val) return root;
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right) return root;
+        else if (left && !right) return left;
+        else if (!left && right) return right;
+
+        return NULL;
+    }
+};
+// @lc code=end
+
