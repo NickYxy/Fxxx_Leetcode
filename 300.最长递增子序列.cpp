6b1ebdd8756e6ffe631d85ac1395f02a57ea3841
Nick <bing900713@hotmail.com>
2023-03-02 13:04:07 +0800

    单调队列，左右开弓+二分

---
+++ 300.最长递增子序列.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=cpp
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> q;
+        for (auto num : nums) {
+            if (q.empty() || num > q.back()) q.push_back(num);
+            else {
+                if (num <= q[0]) q[0] = num;
+                else {
+                    int l = 0, r = q.size() - 1;
+                    while (l < r) {
+                        int mid = (l + r + 1) >> 1;
+                        if (q[mid] < num) l = mid;
+                        else r = mid - 1;
+                    }
+                    q[r + 1] = num;
+                }
+            }
+        }
+        return q.size();
+    }
+};
+// @lc code=end
+
