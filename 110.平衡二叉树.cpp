0575aa33a7537baa33b6169aa18a9c894b2a91e5
Nick <bing900713@hotmail.com>
2023-03-03 22:43:09 +0800

    剪枝思想优化

--- 110.平衡二叉树.cpp
+++ 110.平衡二叉树.cpp
@@ -28,11 +28,24 @@ public:
         int rh = getHeight(root->right);
         if (lh >= 0 && rh >= 0 && abs(lh - rh) <= 1) {
             return max(lh, rh) + 1;
-        } else return -1;
+        } else return -1; // 提前裁剪
 
     }
 };
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) return true;
+        if (abs(getDepth(root->left) - getDepth(root->right)) > 1) return false;
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+    int getDepth(TreeNode* node) {
+        if (!node) return 0;
+        int left  = getDepth(node->left);
+        int right = getDepth(node->right);
 
-
+        return max(left, right) + 1;
+    }
+};
 // @lc code=end
 
