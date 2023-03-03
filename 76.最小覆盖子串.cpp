2c26170c13bb9fd7e7dc04978386c0beb97e2efb
Nick <bing900713@hotmail.com>
2023-03-03 16:40:51 +0800

    比较典型的构造二叉树问题，主要是确定范围

--- 76.最小覆盖子串.cpp
+++ 76.最小覆盖子串.cpp
@@ -8,7 +8,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int m = s.size(), n = t.size();
+        if (m < n) return "";
+        if (s == t) return s;
+        unordered_map<char, int> hs;
+        unordered_map<char, int> ht;
+        for (int i = 0; i < n; i ++) ht[t[i]] ++;
+        int count = 0; //
+        string res;
 
+        for (int i = 0, j = 0; i < m; i++) {
+            hs[s[i]] ++;
+            if (hs[s[i]] <= ht[s[i]]) count ++;
+
+            while (hs[s[j]] > ht[s[j]]) {
+                hs[s[j]] --;
+                j ++;
+            }
+
+            if (count == t.size()) {
+                if (res.empty() || (i - j +1) < res.size()) {
+                    res = s.substr(j, i - j + 1);
+                }
+            }
+        }
+
+        return res;
     }
 };
 // @lc code=end
