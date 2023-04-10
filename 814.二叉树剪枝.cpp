553e0d117767f114591b804cc87f7fa95de0b9ba
Nick <bing900713@hotmail.com>
2023-04-10 10:46:09 +0800

    剪枝就是把自己剪掉，很巧妙的递归剪枝思想，值得多思考多练

---
+++ 814.二叉树剪枝.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=814 lang=cpp
+ *
+ * [814] 二叉树剪枝
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (!root) return nullptr;
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        // dfs，移除条件left==null,right==null,node.val==0;
+        if (!root->left && root->val == 0 && !root->right) return nullptr;
+
+        return root;
+    }
+};
+// @lc code=end
+
