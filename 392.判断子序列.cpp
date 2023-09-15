f124a8497ede4b78ea87f934e117879b9b6d3397
Nick <bing900713@hotmail.com>
2023-09-15 17:12:10 +0800

    简单指针，可用DP优化

---
+++ 392.判断子序列.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=392 lang=cpp
+ *
+ * [392] 判断子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        if (s.size() == 0) return true;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) i++;
+            if (i == s.size()) return true;
+            j ++;
+        }
+
+        return false;
+    }
+};
+// @lc code=end
+
