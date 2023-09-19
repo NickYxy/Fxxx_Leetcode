f5f9813bd5ae7c553da3de37c83f615978ad94a1
Nick <bing900713@hotmail.com>
2023-09-19 11:53:13 +0800

    对树形DP有进一步的了解

--- 337.打家劫舍-iii.cpp
+++ 337.打家劫舍-iii.cpp
@@ -18,6 +18,23 @@
  */
 class Solution {
 public:
+    unordered_map<TreeNode*, int> umap;
+    int rob(TreeNode* root) {
+        auto[root_rob, root_not_rob] = dfs(root);
+        return max(root_rob, root_not_rob);
+    }
+
+    pair<int, int> dfs(TreeNode* node) {
+        if (!node) return {0, 0};
+        auto [l_rob, l_not_rob] = dfs(node->left);
+        auto [r_rob, r_not_rob] = dfs(node->right);
+        int rob = l_not_rob + r_not_rob + node->val;
+        int not_rob = max(l_rob, l_not_rob) + max(r_rob, r_not_rob);
+        return {rob, not_rob};
+    }
+};
+class Solution {
+public:
     int rob(TreeNode* root) {
         vector<int> ans = dfs(root);
         return max(ans[0], ans[1]);
