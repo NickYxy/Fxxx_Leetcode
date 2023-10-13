462ee5583bdf6e106ee2a0dc71a572b93638027f
Nick <bing900713@hotmail.com>
2023-10-13 23:03:18 +0800

    模拟+数据结构

---
+++ 1488.避免洪水泛滥.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=1488 lang=cpp
+ *
+ * [1488] 避免洪水泛滥
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+        vector<int> ans(n, 1);
+        set<int> st;
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < n; ++ i) {
+            if (rains[i] == 0) {
+                st.insert(i);
+            } else {
+                ans[i] = -1;
+                if (mp.count(rains[i])) {
+                    auto it = st.lower_bound(mp[rains[i]]);
+                    if (it == st.end()) {
+                        return {};
+                    }
+                    ans[*it] = rains[i];
+                    st.erase(it);
+                }
+                mp[rains[i]] = i;
+            }
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
