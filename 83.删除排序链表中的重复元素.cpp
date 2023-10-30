38d062befbe0782e477b5a79ec9fcb59afdc8cef
Nick <bing900713@hotmail.com>
2023-10-30 19:38:11 +0800

    逆转思路，不判相等判不相等

---
+++ 83.删除排序链表中的重复元素.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=cpp
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next) return head;
+        auto cur = head;
+        for (auto p = head->next; p; p = p->next) {
+            if (p->val != cur->val) {
+                cur->next = p;
+                cur = p;
+            }
+        }
+
+        cur->next = NULL;
+        return head;
+    }
+};
+// @lc code=end
+
